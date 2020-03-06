daa83680ae124c79a7f42f20d1597f4347d9cf74
Sakshi <sakshi@webintensive.com>
2020-03-06 19:03:53 +0530

    [Added duplicate url check fix.]

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const argv = require("yargs");
 var rp = require('request-promise');
 const path = require('path');
 const url = get('[2]', process.argv);
+const savedUrls = require("./savedUrls");
 console.log(url);
 
 async function getTitle(url) {
@@ -14,12 +15,27 @@ async function getTitle(url) {
 		return matched[1];
 	})
 }
+
 getTitle(url).then(res => {
+	if (existance(url)) {
+		console.log("This url is already saved.");
+		return;
+	}
 	const text = `[${res}](${url})  \n`;
 	writeToFile(text);
+	saveUrl(url);
 })
 
 function writeToFile(text) {
 	const file = path.join(__dirname, 'content.md');
 	fs.appendFileSync(file, text)
+}
+
+function existance(url) {
+	return savedUrls.some((savedUrl) => url === savedUrl);
+}
+
+function saveUrl(url) {
+	savedUrls.push(url);
+	fs.writeFileSync('./savedUrls.json', JSON.stringify(savedUrls, null, 2));
 }
