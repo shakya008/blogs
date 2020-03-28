0b203e03cd866e951b593bb25ff0535e8769c839
Shyam <shyam@webintensive.com>
2020-03-28 23:20:50 +0530

    Added options to ask

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# blogs
+#Web Optimization
+[Lazy Loading Images – The Complete Guide](https://imagekit.io/blog/lazy-loading-images-complete-guide/#subscribe)  
+[Efficiently load JavaScript with defer and async](https://flaviocopes.com/javascript-async-defer)  
+#Node Modules
+[request-promise  -  npm](https://www.npmjs.com/package/request-promise)  
--- index.js
+++ index.js
@@ -1,41 +1,92 @@
-const {pipe, get }= require("lodash/fp");
+const { pipe, get, groupBy, forOwn, map } = require("lodash/fp");
 const fs = require("fs");
 const readline = require("readline");
 const argv = require("yargs");
-var rp = require('request-promise');
-const path = require('path');
-const url = get('[2]', process.argv);
+var rp = require("request-promise");
+const path = require("path");
+const url = get("[2]", process.argv);
 const savedUrls = require("./savedUrls");
-console.log(url);
+const types = require("./types");
+const store = require("./store");
 
 async function getTitle(url) {
 	return rp(url).then(htmlString => {
 		const matched = htmlString.match(/<title\s*.*?>(.*?)<\/title>/);
-		console.log(matched[1]);
 		return matched[1];
-	})
+	});
 }
 
-getTitle(url).then(res => {
+async function processUrl() {
+	let res = await getTitle(url);
 	if (existance(url)) {
 		console.log("This url is already saved.");
 		return;
 	}
 	const text = `[${res}](${url})  \n`;
-	writeToFile(text);
+	const selectedType = await getType();
+	const val = {
+		type: selectedType,
+		url: url,
+		title: res
+	};
+	store.push(val);
+	writeToStore();
+	generateContent();
 	saveUrl(url);
-})
+}
 
-function writeToFile(text) {
-	const file = path.join(__dirname, 'content.md');
-	fs.appendFileSync(file, text)
+function writeToStore() {
+	fs.writeFileSync("./store.json", JSON.stringify(store, null, 2));
+}
+function generateContent() {
+	const file = path.join(__dirname, "README.md");
+	const text = pipe(
+		groupBy("type"),
+		o => {
+			let str = "";
+			forOwn(function(val) {
+				const type = val[0].type;
+				str =
+					str +
+					`#${type}\n` +
+					map(item => {
+						return `[${item.title}](${item.url})  \n`;
+					}, val).join("");
+			}, o);
+			return str;
+		}
+	)(store);
+	fs.writeFileSync(file, text);
 }
 
 function existance(url) {
-	return savedUrls.some((savedUrl) => url === savedUrl);
+	return savedUrls.some(savedUrl => url === savedUrl);
 }
 
 function saveUrl(url) {
 	savedUrls.push(url);
-	fs.writeFileSync('./savedUrls.json', JSON.stringify(savedUrls, null, 2));
+	fs.writeFileSync("./savedUrls.json", JSON.stringify(savedUrls, null, 2));
 }
+
+async function takeResponse(query) {
+	return new Promise((resolve, reject) => {
+		var rl = readline.createInterface({
+			input: process.stdin,
+			output: process.stdout
+		});
+		rl.question(query, res => {
+			rl.close();
+			resolve(res);
+		});
+	});
+}
+
+async function getType(argument) {
+	const options = types.map((type, i) => `${i} ${type}\n`).join("");
+	const selectedType = await takeResponse(
+		`Select type to add in \n${options}`
+	);
+	return types[selectedType];
+}
+
+processUrl().then();
